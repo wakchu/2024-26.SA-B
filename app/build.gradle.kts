788e4621cf930591d06b8bcc0a59c620c97f5cfd
aa <a.aglietti@itsrizzoli.it>
2025-10-31 00:34:06 +0100

    test: run app in avd during instrumented test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,4 +60,5 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.androidx.espresso.intents)
 }
--- app/src/androidTest/java/com/example/androidintermedio/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/androidintermedio/ExampleInstrumentedTest.kt
@@ -1,12 +1,10 @@
 package com.example.androidintermedio
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +13,13 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    /**
+     * https://developer.android.com/training/testing/espresso/setup#add-first-test
+     */
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.androidintermedio", appContext.packageName)
     }
 }
